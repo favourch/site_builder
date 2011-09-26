e2a1777113f1b88e5ccb92bb6dd0f417830ba15b
Jon Stoddart <stoddart.jon@gmail.com>
2011-09-25 19:46:50 -0600

    Version 2.0.4. Updated new_version script.

--- new_version.php
+++ new_version.php
@@ -1,5 +1,6 @@
 <?php
 
+// Sorts versions numerically. Regular PHP sort function is unable to sort with type x.x.x numerically.
 function sortVersions($v1, $v2)
 {
 	$tmp1 = array_merge(explode('.', $v1), array(0, 0, 0));
@@ -20,6 +21,7 @@ function sortVersions($v1, $v2)
 	return 1;
 }
 
+// Get the last version created.
 function getLastVersion($path='')
 {
 	$version_list = array();
@@ -34,7 +36,9 @@ function getLastVersion($path='')
 	return end($version_list);
 }
 
-function copyDir($last_dir='', $new_dir='', $update_dir='')
+// Check each file in a directory and copy that file to "update" directory if it's new. If a directory
+// is found, recursively call this function for each directory.
+function copyDir($last_dir='', $new_dir='', $update_dir='', &$file_list)
 {
 	$dh = opendir($new_dir);
 	while($file = readdir($dh)) {
@@ -45,7 +49,7 @@ function copyDir($last_dir='', $new_dir='', $update_dir='')
 		$new_file = "$new_dir/$file";
 		$update_file = "$update_dir/$file";
 		if(is_dir($new_file)) {
-			copyDir($last_file, $new_file, $update_file);
+			copyDir($last_file, $new_file, $update_file, $file_list);
 		}
 		else {
 			$copy = false;
@@ -53,6 +57,7 @@ function copyDir($last_dir='', $new_dir='', $update_dir='')
 				if(!is_dir($update_dir)) {
 					makeDir($update_dir);
 				}
+				$file_list[] = 'N '.str_replace(dirname(__FILE__).'/current/', '', $new_file);
 				exec("cp $new_file $update_file");
 				echo "N $new_file\r\n";
 			}
@@ -60,6 +65,7 @@ function copyDir($last_dir='', $new_dir='', $update_dir='')
 				if(!is_dir($update_dir)) {
 					makeDir($update_dir);
 				}
+				$file_list[] = 'U '.str_replace(dirname(__FILE__).'/current/', '', $new_file);
 				exec("cp $new_file $update_file");
 				echo "U $new_file\r\n";
 			}
@@ -67,6 +73,8 @@ function copyDir($last_dir='', $new_dir='', $update_dir='')
 	}
 }
 
+// Make a new directory in "update". If the parent directory does not exist, call this function recursively
+// to create all necessary parent directories.
 function makeDir($dir)
 {
 	$parts = explode('/', $dir);
@@ -80,6 +88,7 @@ function makeDir($dir)
 	}
 }
 
+// Convert any minor/major versions (x.x) to full version number for incrementation (x.x.x).
 function fillVersion($v)
 {
 	$tmp = explode('.', $v);
@@ -90,30 +99,60 @@ function fillVersion($v)
 	return implode('.', $tmp);
 }
 
+// Update the version number shown in "base_dm" class.
+function updateVersionNumber($version)
+{
+	$file = dirname(__FILE__).'/current/includes/system/class.base_dm.php';
+	if(!is_file($file)) {
+		die("Unable to find base_dm class at $file");
+	}
 
+	$fh = fopen($file, 'r');
+	$text = fread($fh, filesize($file));
+	fclose($fh);
+
+	$start = strpos($text, 'DATA_MODELER_VERSION') + 22;
+	$start = strpos($text, "'", $start) + 1;
+	$end = strpos($text, "'", $start);
+
+	$newText = substr($text, 0, $start) . $version . substr($text, $end);
+
+	$fh = fopen($file, 'w');
+	fwrite($fh, $newText);
+	fclose($fh);
+}
+
+
+// Must specify either a specific version, "last" (display last version), "major" (new major version),
+// "minor" (new minor version), or "bug" (new bug fix version).
 if($argc <= 1) {
 	die("Please specify the version to create.\r\n");
 }
 
+// Get the new and last version.
 $version = $argv[1];
 $path = dirname(__FILE__).'/versions';
 $last_version = getLastVersion($path);
 
+// Display the last version, then quit.
 if($version == 'last') {
 	die("The last version is $last_version.\r\n");
 }
+// Get the version number for a new major version.
 elseif($version == 'major') {
 	$v = fillVersion($last_version);
 	$tmp = explode('.', $v);
 	$tmp[0]++;
 	$version = "$tmp[0].0";
 }
+// Get the version number for a new minor version.
 elseif($version == 'minor') {
 	$v = fillVersion($last_version);
 	$tmp = explode('.', $v);
 	$tmp[1]++;
 	$version = "$tmp[0].$tmp[1]";
 }
+// Get the version number for a new bug version.
 elseif($version == 'bug') {
 	$v = fillVersion($last_version);
 	$tmp = explode('.', $v);
@@ -121,23 +160,47 @@ elseif($version == 'bug') {
 	$version = "$tmp[0].$tmp[1].$tmp[2]";
 }
 
+// Make sure this version doesn't already exist.
 $new_dir = "$path/$version";
 if(is_dir($new_dir)) {
 	die("Version $version already exists.\r\n");
 }
 
+// Update "base_dm" class to show new version number.
+updateVersionNumber($version);
+
+// Create the base directories for the version.
 echo "Creating version $version.\r\n";
 mkdir($new_dir);
 mkdir("$new_dir/new");
 mkdir("$new_dir/update");
 
+// Copy all files to "new" directory for any new installations.
 echo "Copying all files to \"new\" directory.\r\n";
 exec("cp -R ./current/* $new_dir/new/");
 
+// If there is a version prior to this, find and copy all updated files to the "update" directory.
 if($last_version) {
+	$file_list = array();
 	echo "Copying all updated/new files to \"update\" directory. Review the list below to ensure accuracy. (N=new, U=updated)\r\n";
-	copyDir("$path/$last_version/new", dirname(__FILE__).'/current', "$new_dir/update");
+	copyDir("$path/$last_version/new", dirname(__FILE__).'/current', "$new_dir/update", $file_list);
+
+	$files_string = implode("\n", $file_list);
+	$update_text = <<<EOT
+This is a list of files that were new or updated in version $version.
+N = New file
+U = Updated file
+
+$files_string
+
+EOT;
+	$update_text = str_replace("\r\n", "\n", $update_text);
+
+	$fh = fopen("$new_dir/files.txt", 'w');
+	fwrite($fh, $update_text);
+	fclose($fh);
 }
+// If this is the first version, copy all files to the "update" directory.
 else {
 	echo "This is the first version. Copying all files to \"update\" directory.\r\n";
 	exec("cp -R ./current/* $new_dir/update/");
---
+++ versions/2.0.4/new/includes/system/build_model.php
@@ -0,0 +1,97 @@
+<?php
+
+include_once('../config.php');
+
+function className($table)
+{
+	$str = str_replace('_', ' ', $table);
+	$str = ucwords($str);
+	$str = str_replace(' ', '', $str);
+	return $str;
+}
+
+echo 'Building models...<br />
+<div style="padding: 0px 0px 0px 15px">
+';
+
+$sql = "show tables";
+foreach($dm->db()->get_results($sql) as $row)
+{
+	$table	= $row->{'Tables_in_'.$dm->dbInfo()->name()};
+	$sql	= "describe `$table`";
+	$fields	= $dm->db()->get_results($sql);
+	$edit	= array();
+
+	/**** BASE MODEL CLASS FILE ****/
+	$fieldDecl = '';
+	foreach($fields as $field) {
+		$fieldDecl .= "	protected \$$field->Field; // Type: $field->Type, Null: $field->Null, Key: $field->Key\r\n";
+		$edit[]		= $field;
+	}
+	array_shift($edit);
+
+	$fieldArr = "\r\n";
+	foreach($edit as $field) {
+		$fieldArr .= "\t\t\t\t'$field->Field'=>'$field->Field',\r\n";
+	}
+	$fieldArr .= "\t\t\t";
+
+	$filetext = "<?php
+
+class Base".className($table)." extends data_util
+{
+$fieldDecl
+
+	function Base".className($table)."(\$object=null)
+	{
+		\$this->__tableName	= '$table';
+		\$this->__idField	= '".$fields[0]->Field."';
+		\$this->__fields	= array($fieldArr);
+	}
+}
+
+";
+
+	$filename = $dm->basePath().'includes/model/base/class.Base'.className($table).'.php';
+
+	$fp = fopen($filename, 'w');
+	fwrite($fp, $filetext);
+	fclose($fp);
+
+	echo "$filename created.<br />\r\n";
+	/**** END BASE MODEL CLASS FILE ****/
+
+
+
+
+	/**** EXTENDED MODEL CLASS FILE ****/
+	$filename = $dm->basePath().'includes/model/class.'.className($table).'.php';
+
+	if(!file_exists($filename)) {
+		$filetext = "<?php
+
+class ".className($table)." extends Base".className($table)."
+{
+	function ".className($table)."(\$object=null)
+	{
+		parent::__construct(\$object);
+	}
+}
+
+";
+
+		$fp = fopen($filename, 'w');
+		fwrite($fp, $filetext);
+		fclose($fp);
+
+		echo "$filename created.<br />\r\n";
+	}
+	else {
+		echo "$filename exists.<br />\r\n";
+	}
+	/**** END EXTENDED MODEL CLASS FILE ****/
+}
+
+echo "</div>\r\nDone.<br />\r\n";
+
+?>
